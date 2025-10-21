92e7106a87b9e3d93d88b4f75a5d68c4a3632295
HirushaReshan <hiru.resh@gmail.com>
2025-10-22 00:49:19 +0530

    Added User and Business Drawer

---
+++ lib/components/drawer/business_drawer.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class BusinessDrawer extends StatelessWidget {
+  const BusinessDrawer({super.key});
+
+  //sginout User
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.grey.shade300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //drawer Header
+          Column(
+            children: [
+              DrawerHeader(
+                child: Icon(
+                  Icons.business,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+
+              //Home tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.home, color: Colors.grey),
+                  title: Text('H O M E'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              //Profile Tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.group, color: Colors.grey),
+                  title: Text('B U S S I N E S S \n P R O F I L E'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.animation, color: Colors.grey),
+                  title: Text('A N O T H E R'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              //contact us
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.call, color: Colors.grey),
+                  title: Text('C O N T A C T  U S'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/contact_us_page');
+                  },
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.settings, color: Colors.grey),
+                  title: Text('Business S E T T I N G S'),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/business_settings_page');
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          //Logout Button
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.logout, color: Colors.grey),
+              title: Text('LOG OUT'),
+              onTap: () {
+                //pop the darwer
+                Navigator.pop(context);
+
+                //signout the User
+                signUserOut();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
